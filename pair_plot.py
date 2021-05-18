5a37bdd6f13fd799b7b911abd8b45a29c591af55
Said Bahaddi <said.bahaddi@gmail.com>
2021-05-18 13:25:07 +0100

    pair plot finished

--- pair_plot.py
+++ pair_plot.py
@@ -2,14 +2,34 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 import argparse
+import os
 
-# From this visualization, what features are you going to use for your logistic regression?
 
-# Features: "Arithmancy and care of magic creatures" are homogenous across the classes and can be eliminated
-# "Astronomy and Defense against the dark arts are correlated", this means we can eliminate one of them.
-
-# so we eliminate Arithmancy, care of magic creatures and Astronomy
-# Eliminating features speeds up the algorithm
+def save_pairPlots(M_data, B_data):
+    fig, ax = plt.subplots(30, 30, figsize=(30, 30))
+    for i in range(0, n_feature):
+        for j in range(0, n_feature):
+            if (i == j):
+                ax[i][j].hist(M_data[:, i].tolist(),
+                              label="Malignant", fill=True, alpha=0.5)
+                ax[i][j].hist(B_data[:, i].tolist(),
+                              label="Benign", fill=True, alpha=0.5)
+            else:
+                ax[i][j].scatter(M_data[:, i].tolist(), M_data[:,
+                                                               j].tolist(), label="Malignant", alpha=0.5,)
+                ax[i][j].scatter(B_data[:, i].tolist(), B_data[:,
+                                                               j].tolist(), label="Benign", alpha=0.5,)
+            if i == 0:
+                ax[i][j].set_title("feature " + str(j + 1))
+            if j == 0:
+                ax[i][j].set_ylabel("feature " + str(i + 1))
+            ax[i][j].set_xticks([])
+            ax[i][j].set_yticks([])
+            print('creating pair plot feature %02d vs feature %02d ' %
+                  (i + 1, j + 1))
+    fig.tight_layout()
+    print("Pair Plot saved..")
+    plt.savefig("pair_plots/pair_plot.png")
 
 
 if __name__ == "__main__":
@@ -25,12 +45,17 @@ if __name__ == "__main__":
         data = pd.read_csv(
             args.filename, header=None, usecols=[i for i in range(1, 32)]
         )
-        #data = data.to_numpy()
+        data = data.to_numpy()
     except Exception:
         print("Can't open file.")
         exit()
 
-    # print(data[1])
-    sns.set_theme(font_scale=0.63)
-    g = sns.pairplot(data, hue=1, palette="husl", height=1.1)
-    plt.show()
+    n_feature = len(data[0]) - 1
+    B_data = data[data[:, 0] == "M"][:, 1:]
+    M_data = data[data[:, 0] == "B"][:, 1:]
+
+    if not os.path.exists("pair_plots/"):
+        os.mkdir("pair_plots/")
+
+    print("pair plots will be saved as image in 'pair_plots/' folder")
+    save_pairPlots(M_data, B_data)
